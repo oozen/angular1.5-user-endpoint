91a35e788b06ef87bbb25c1bd183445e055e6dba
ozgrozn <ozgrozn79@gmail.com>
2017-01-12 17:42:59 +0300

    Created initial files and folders

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+﻿var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('angular', function () {
+    gulp.src('node_modules/angular/angular.min.js').pipe(gulp.dest('src/js'));
+});
